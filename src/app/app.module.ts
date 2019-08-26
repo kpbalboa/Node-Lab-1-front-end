39122075b36e450166d6708443b475510f26d24d
kevin balboa <kpbalboa@gmail.com>
2019-08-26 09:01:48 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {CartService} from './cart.service';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { HttpClientModule } from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import {RouterModule, Routes} from '@angular/router';
+import { FormComponent } from './products/form/form.component';
+
+const appRoutes: Routes=[];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    FormComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  imports: [BrowserModule, HttpClientModule, FormsModule, RouterModule.forRoot(appRoutes)],
+  providers: [CartService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
